7c8ee00ff9823f71aacd993ed1a1d6c03080b02f
zosimadis <ensozos@hotmail.com>
2018-10-02 19:31:51 +0300

    Step 2: mining segment-specific frequent itemsets

--- src/main/scala/TargetedPromotionSpark.scala
+++ src/main/scala/TargetedPromotionSpark.scala
@@ -1,6 +1,9 @@
+import org.apache.spark.{Partition, TaskContext}
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.ml.linalg.SparseVector
+import org.apache.spark.mllib.fpm.FPGrowth
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
@@ -9,8 +12,8 @@ import scala.collection.mutable
 object TargetedPromotionSpark {
 
 
-  val NUMBER_ITERATIONS: Int = 10
-  val K = 4
+  val NUMBER_ITERATIONS: Int = 50
+  val K = 1
 
   def main(args: Array[String]): Unit = {
 
@@ -24,13 +27,13 @@ object TargetedPromotionSpark {
 
     import ss.implicits._
 
-    val categoriesDF = ss.read.option("header", "true").csv(categoriesPath)
+    val categoriesMap = ss.read.option("header", "true").csv(categoriesPath).collect().map(r => (r.getString(0), r.getString(1))).toMap
     val productsDF = ss.read.option("header", "true").csv(productsPath)
     val salesDF = ss.read.option("header", "true").csv(transactionsPath).
       drop("promotion_id", "store_id", "store_sales", "store_cost", "unit_sales")
 
     val productsMap = productsDF.collect().map(r => (r.getString(0), r.getString(1))).toMap
-    val bcategoriesSize = ss.sparkContext.broadcast(categoriesDF.collect().size)
+    val bcategoriesSize = ss.sparkContext.broadcast(categoriesMap.size)
     val bproductMap = sc.broadcast(productsMap)
 
     //transactions
@@ -87,7 +90,53 @@ object TargetedPromotionSpark {
       i += 1
     }
 
-    println(bcenters.value)
+    val customerClusterMap = customerMeanTransactionsRDD.map(r => {
+      val min_center = bcenters.value.map(c => (c._1, 1 - Utils.tanimoto(c._2, r._2)))
+        .minBy(_._2)
+
+      (r._1, min_center._1)
+    }).collect().toMap
+    val bcustomerClusterMap = sc.broadcast(customerClusterMap)
+
+    val transactionsFPG = transactionsRDD.map(r => {
+      var clusterID = bcustomerClusterMap.value.get(r._1.split(':')(1)).get
+      var transactions = Array(r._2.indices.map(r => r.toString))
+
+      (clusterID, transactions)
+    }).reduceByKey((x, y) => x ++ y)
+      .persist()
+
+
+    val minConfidence = 0.6
+    var clusterID = 0
+    for (clusterID <- 0 to K - 1) {
+
+      val clusterTransactionsRDD = transactionsFPG.filter(r => r._1 == clusterID).flatMap(r => r._2)
+
+      val fpg = new FPGrowth()
+        .setMinSupport(0.1)
+        .setNumPartitions(10)
+
+      val model = fpg.run(clusterTransactionsRDD)
+
+      model.freqItemsets.collect().foreach { itemset =>
+        var catItems = itemset.items.map(r => categoriesMap.get((r.toInt + 1).toString).get)
+        println(s"${catItems.mkString("[", ",", "]")},${itemset.freq}")
+      }
+
+      model.generateAssociationRules(minConfidence).collect().foreach { rule =>
+        var anteCatItems = rule.antecedent.map(r => categoriesMap.get((r.toInt + 1).toString).get)
+        var conseCatItems = rule.consequent.map(r => categoriesMap.get((r.toInt + 1).toString).get)
+
+        println(s"${anteCatItems.mkString("[", ",", "]")}=> " +
+          s"${conseCatItems.mkString("[", ",", "]")},${rule.confidence}")
+      }
+
+      println("END OF FPGROWTH")
+      println("cluster id = " + clusterID)
+      println("count = " + clusterTransactionsRDD.count())
+
+    }
 
   }
 

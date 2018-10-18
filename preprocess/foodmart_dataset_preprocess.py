d3cfff50383a2236f7be3251ddf1e364720a0bd7
Nikolaos Kapraras <iamnikosk93@gmail.com>
2018-10-18 13:12:52 +0300

    added filtering options for preprocessing

--- preprocess/foodmart_dataset_preprocess.py
+++ preprocess/foodmart_dataset_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import argparse
 
@@ -7,8 +8,8 @@ import argparse
 FoodMart dataset preprocessing
 
 Download FoodMart dataset from https://goo.gl/WDMoJJ and export
-the 'product' and 'product_class' tables as csv. After that, use the
-resulting csv files as input for this script.
+the 'product', 'product_class' and 'sales_fact_1997' tables as csv. 
+After that, use the resulting csv files as input for this script.
 '''
 
 # global constants
@@ -19,12 +20,16 @@ CATEGORIES_OUT_HEADER = 'category_id,category_name\n'
 # default options
 dopt_classes_in_fname = './input/product_class.csv'
 dopt_products_in_fname = './input/product.csv'
+dopt_sales_in_fname = './input/sales_fact_1997.csv'
 dopt_products_out_fname = './output/foodmart_products.csv'
 dopt_categories_out_fname = './output/foodmart_categories.csv'
 dopt_classes_columns = ['product_subcategory', 'product_category', 'product_department']
 
 def unique(items):
     return list(set(items))
+
+def diff(a, b):
+    return list(set(a) - set(b))
 
 def keep_valid_columns(columns):
     return list(set(columns) & set(VALID_CLASSES_COLUMNS))
@@ -35,13 +40,45 @@ def create_parser():
 
     parser.add_argument('--classes_in', default=dopt_classes_in_fname)
     parser.add_argument('--products_in', default=dopt_products_in_fname)
+    parser.add_argument('--sales_in', default=dopt_sales_in_fname)
     parser.add_argument('--products_out', default=dopt_products_out_fname)
     parser.add_argument('--categories_out', default=dopt_categories_out_fname)
     parser.add_argument('-c', '--classes_columns', nargs='+', default=dopt_classes_columns)
     parser.add_argument('-d', '--classes_depth', type=int)
     parser.add_argument('-z', '--zero_index', action='store_true')
+    parser.add_argument('-f', '--filter', type=int)
+    parser.add_argument('-x', '--skip_filtered', action='store_true')
+    parser.add_argument('-p', '--plot', action='store_true')
 
     return parser
+
+def write_products(args, products_extended_df, categories, index_diff):
+    with open(args.products_out, 'w+') as file:
+        file.write(PRODUCTS_OUT_HEADER)
+
+        for index, row in products_extended_df.iterrows():
+            product_row = []
+            has_filtered = False
+
+            for col in args.classes_columns:
+                if row[col] in categories:
+                    product_row.append(categories.index(row[col]) + index_diff)
+                else:
+                    has_filtered = True
+            product_row = map(lambda i: str(i), unique(product_row))
+
+            if args.skip_filtered and has_filtered:
+                product_row = []
+
+            file.write('{},{}\n'.format(
+                str(row.product_id), '-'.join(product_row)))
+
+def write_categories(args, categories, index_diff):
+    with open(args.categories_out, 'w+') as file:
+        file.write(CATEGORIES_OUT_HEADER)
+        for index in range(len(categories)):
+            file.write('{},{}\n'.format(
+                str(index + index_diff), categories[index]))
 
 def main():
     # parse cli args
@@ -58,7 +95,8 @@ def main():
         exit(1)
 
     # create missing output directories
-    out_dirs = map(lambda f: os.path.dirname(f), [args.products_out, args.categories_out])
+    out_dirs = map(lambda f: os.path.dirname(
+        f), [args.products_out, args.categories_out])
     for d in out_dirs:
         if not os.path.exists(d):
             os.makedirs(d)
@@ -79,25 +117,37 @@ def main():
 
     # join product with classes and keep relevant columns
     ext_mask = ['product_id', 'product_class_id'] + args.classes_columns
-    products_extended_df = products_df.merge(classes_df, on='product_class_id').sort_values(['product_id'])[ext_mask]
+    products_extended_df = products_df.merge(
+        classes_df, on='product_class_id').sort_values(['product_id'])[ext_mask]
+
+    if args.filter != None or args.plot:
+        # load sales table and join with products
+        sales_df = pd.read_csv(args.sales_in)
+        sales_extended_df = sales_df.merge(products_extended_df, on='product_id')
+
+        # group and count subcategory frequencies
+        subcategory_count_df = sales_extended_df[['product_subcategory', 'product_id']].groupby(
+            'product_subcategory', as_index=False).count().sort_values(['product_id'])
+
+        # filter subcategories with high frequencies
+        if args.filter != None:
+            comp = subcategory_count_df['product_id'] >= args.filter
+            high_freq_df = subcategory_count_df.where(comp).dropna(axis=0)
+            blacklist = high_freq_df.product_subcategory.unique().tolist()
+
+            # update categories
+            categories = diff(categories, blacklist)
+
+        # show plot
+        if args.plot:
+            subcategory_count_df.plot(kind='bar')
+            plt.show()
 
     # save products x categories file
-    with open(args.products_out, 'w+') as file:
-        file.write(PRODUCTS_OUT_HEADER)
-
-        for index, row in products_extended_df.iterrows():
-            product_row = []
-            for col in args.classes_columns:
-                product_row.append(categories.index(row[col]) + index_diff)
-            product_row = map(lambda i: str(i), unique(product_row))
-
-            file.write('{},{}\n'.format(str(row.product_id), '-'.join(product_row)))
+    write_products(args, products_extended_df, categories, index_diff)
 
     # save categories file
-    with open(args.categories_out, 'w+') as file:
-        file.write(CATEGORIES_OUT_HEADER)
-        for index in range(len(categories)):
-            file.write('{},{}\n'.format(str(index + index_diff), categories[index]))
+    write_categories(args, categories, index_diff)
 
 if __name__ == '__main__':
     main()
